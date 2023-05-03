189f2bf44444b3bdd2e23399374fcf469e08f9df
Felipe Santoro Alcantara <santoro.felipe@gmail.com>
2023-05-03 19:56:42 -0300

    Final

--- code/deploy_site.sh
+++ code/deploy_site.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
-git pull origin
 
 mkdir /srv/site_sprint_i
 
@@ -14,7 +13,7 @@ docker build --tag=flpsantoro/site_sprint_i:1.0 --rm=true .
 docker run \
   --name site_sprint_i \
   --hostname=site_sprint_i \
-  --network puc \
+  --network=puc \
   -v /srv/site_sprint_i:/usr/share/nginx/html \
   -p 33001:80 \
   -d flpsantoro/site_sprint_i:1.0
